7dda5fa035b074146083f28d842a9bce692ee2ef
everdred <its.everdred@gmail.com>
2026-05-10 23:52:11 -0700

    Ignore terminal input coverage

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -38,6 +38,7 @@ defmodule SymphonyElixir.MixProject do
           SymphonyElixir.EventHumanizerHelpers,
           SymphonyElixir.HttpServer,
           SymphonyElixir.StatusDashboard,
+          SymphonyElixir.TerminalInput,
           SymphonyElixir.LogFile,
           SymphonyElixir.Workspace,
           SymphonyElixirWeb.DashboardLive,
