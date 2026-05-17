aa12ce9cb52c3b7ae35872cb756a4dc88598f68a
everdred <its.everdred@gmail.com>
2026-05-16 21:21:47 -0700

    Add Owl dep

--- elixir/mix.exs
+++ elixir/mix.exs
@@ -91,6 +91,7 @@ defmodule SymphonyElixir.MixProject do
       {:yaml_elixir, "~> 2.12"},
       {:solid, "~> 1.2"},
       {:ecto, "~> 3.13"},
+      {:owl, "~> 0.13"},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ]
