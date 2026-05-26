4b4ecfac6e7bc72998fa1d1e309344072936df21
everdred <kevinw@oplabs.co>
2026-05-25 21:05:04 -0700

    Add event flow c

--- elixir/test/aiur/sandbox/event_flow_demo_test.exs
+++ elixir/test/aiur/sandbox/event_flow_demo_test.exs
@@ -10,4 +10,10 @@ defmodule Aiur.Sandbox.EventFlowDemoTest do
   test "function_b returns one more than function_a" do
     assert EventFlowDemo.function_b() == EventFlowDemo.function_a() + 1
   end
+
+  test "function_c squares function_b" do
+    result = EventFlowDemo.function_b()
+
+    assert EventFlowDemo.function_c() == result * result
+  end
 end
