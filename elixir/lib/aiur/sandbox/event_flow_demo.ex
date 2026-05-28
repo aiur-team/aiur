5824b74365466bd45ab008e708d844f92dbde057
everdred <kevinw@oplabs.co>
2026-05-27 18:22:45 -0700

    Add event flow function

---
+++ elixir/lib/aiur/sandbox/event_flow_demo.ex
@@ -0,0 +1,11 @@
+defmodule Aiur.Sandbox.EventFlowDemo do
+  @moduledoc """
+  Sandbox helpers for the event-flow manual test tickets.
+  """
+
+  @doc """
+  Returns the fixed value for event-flow ticket #1.
+  """
+  @spec function_a() :: 42
+  def function_a, do: 42
+end
