d92af5aef743ac4a403a951c8f4a4867c6926c01
everdred <kevinw@oplabs.co>
2026-05-28 22:43:07 -0700

    Add function b demo

--- elixir/test/aiur/sandbox/event_flow_demo_test.exs
+++ elixir/test/aiur/sandbox/event_flow_demo_test.exs
@@ -6,4 +6,8 @@ defmodule Aiur.Sandbox.EventFlowDemoTest do
   test "function_a/0 returns the sandbox sentinel value" do
     assert EventFlowDemo.function_a() == 42
   end
+
+  test "function_b calls function_a and adds one" do
+    assert EventFlowDemo.function_b() == 43
+  end
 end
