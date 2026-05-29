c68ffff65bbf0b1dc88be16df6ae6290c8617f51
everdred <kevinw@oplabs.co>
2026-05-29 10:19:03 -0700

    Add function c demo

--- elixir/test/aiur/sandbox/event_flow_demo_test.exs
+++ elixir/test/aiur/sandbox/event_flow_demo_test.exs
@@ -6,4 +6,8 @@ defmodule Aiur.Sandbox.EventFlowDemoTest do
   test "function_b/0 calls function_a/0 and adds one" do
     assert EventFlowDemo.function_b() == 43
   end
+
+  test "function_c/0 squares function_b/0" do
+    assert EventFlowDemo.function_c() == 1849
+  end
 end
