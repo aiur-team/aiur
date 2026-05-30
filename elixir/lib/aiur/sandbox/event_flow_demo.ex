2963ef0c27cb37de4d84bcca13dc205d4b413457
everdred <kevinw@oplabs.co>
2026-05-29 22:47:31 -0700

    Add EventFlowDemo function_b/0
    Implements the #100 sandbox slice: function_b/0 calls function_a/0
    (integrated from #99) and adds 1, returning 43.

--- elixir/lib/aiur/sandbox/event_flow_demo.ex
+++ elixir/lib/aiur/sandbox/event_flow_demo.ex
@@ -4,5 +4,9 @@ defmodule Aiur.Sandbox.EventFlowDemo do
   # the test tickets flesh this out during the manual --test workflow;
   # `aiur --test` restores this file to HEAD before each fresh run.
 
+  @spec function_a() :: 42
   def function_a, do: 42
+
+  @spec function_b() :: 43
+  def function_b, do: function_a() + 1
 end
