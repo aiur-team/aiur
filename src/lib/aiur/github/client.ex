9de538b53501d6fb9b25931ce16d676bcff9d51a
everdred <kevinw@oplabs.co>
2026-07-09 08:35:09 -0700

    Extract review thread reads

--- src/lib/aiur/github/client.ex
+++ src/lib/aiur/github/client.ex
@@ -15,6 +15,7 @@ defmodule Aiur.GitHub.Client do
     Issues,
     PullRequests,
     RepoEvents,
+    ReviewThreads,
     StatePolicy,
     Teams,
     Transport
@@ -57,65 +58,6 @@ defmodule Aiur.GitHub.Client do
       thread {
         id
         isResolved
-      }
-    }
-  }
-  """
-
-  @review_thread_query """
-  query AiurReviewThread($id: ID!) {
-    node(id: $id) {
-      ... on PullRequestReviewThread {
-        id
-        isResolved
-        path
-        line
-        comments(last: 20) {
-          nodes {
-            id
-            databaseId
-            body
-            createdAt
-            updatedAt
-            url
-            author {
-              login
-            }
-          }
-        }
-      }
-    }
-  }
-  """
-
-  @unaddressed_review_threads_query """
-  query AiurUnaddressedReviewThreads($owner: String!, $repo: String!, $number: Int!, $cursor: String) {
-    repository(owner: $owner, name: $repo) {
-      pullRequest(number: $number) {
-        reviewThreads(first: 100, after: $cursor) {
-          pageInfo {
-            hasNextPage
-            endCursor
-          }
-          nodes {
-            id
-            isResolved
-            path
-            line
-            comments(last: 20) {
-              nodes {
-                databaseId
-                body
-                createdAt
-                updatedAt
-                url
-                author {
-                  login
-                }
-              }
-            }
-          }
-        }
       }
     }
   }
@@ -243,24 +185,8 @@ defmodule Aiur.GitHub.Client do
 
   @spec fetch_unaddressed_pr_review_thread_comments(String.t() | integer(), keyword()) ::
           {:ok, [map()]} | {:error, term()}
-  def fetch_unaddressed_pr_review_thread_comments(pr_number, opts \\ []) do
-    with {:ok, {owner, repo}} <- Transport.parse_repo(),
-         {:ok, token} <- Transport.require_token(opts),
-         {:ok, number} <- normalize_pr_number(pr_number) do
-      request_fun = Keyword.get(opts, :request_fun, &Transport.default_request_fun/1)
-
-      fetch_unaddressed_review_thread_pages(
-        request_fun,
-        token,
-        owner,
-        repo,
-        number,
-        nil,
-        opts,
-        []
-      )
-    end
-  end
+  def fetch_unaddressed_pr_review_thread_comments(pr_number, opts \\ []),
+    do: ReviewThreads.fetch_unaddressed_pr_review_thread_comments(pr_number, opts)
 
   @spec reply_to_review_thread(String.t(), String.t(), keyword()) ::
           {:ok, map()} | {:error, term()}
@@ -367,79 +293,6 @@ defmodule Aiur.GitHub.Client do
   end
 
   # -- Private helpers --------------------------------------------------------
-
-  defp normalize_pr_number(number) when is_integer(number) and number > 0, do: {:ok, number}
-
-  defp normalize_pr_number(number) when is_binary(number) do
-    case Integer.parse(number) do
-      {parsed, ""} when parsed > 0 -> {:ok, parsed}
-      _ -> {:error, {:invalid_pr_number, number}}
-    end
-  end
-
-  defp normalize_pr_number(number), do: {:error, {:invalid_pr_number, number}}
-
-  defp fetch_unaddressed_review_thread_pages(
-         request_fun,
-         token,
-         owner,
-         repo,
-         number,
-         cursor,
-         opts,
-         acc
-       ) do
-    variables =
-      %{"owner" => owner, "repo" => repo, "number" => number}
-      |> Transport.maybe_put_query("cursor", cursor)
-
-    case Transport.github_graphql(request_fun, token, @unaddressed_review_threads_query, variables) do
-      {:ok, body} ->
-        with {:ok, {threads, page_info}} <- review_threads_page(body) do
-          comments = unaddressed_thread_comments(threads, opts)
-
-          continue_unaddressed_review_thread_pages(
-            request_fun,
-            token,
-            owner,
-            repo,
-            number,
-            page_info,
-            opts,
-            acc ++ comments
-          )
-        end
-
-      {:error, _reason} = error ->
-        error
-    end
-  end
-
-  defp continue_unaddressed_review_thread_pages(
-         request_fun,
-         token,
-         owner,
-         repo,
-         number,
-         page_info,
-         opts,
-         acc
-       ) do
-    if Map.get(page_info, "hasNextPage") == true do
-      fetch_unaddressed_review_thread_pages(
-        request_fun,
-        token,
-        owner,
-        repo,
-        number,
-        Map.get(page_info, "endCursor"),
-        opts,
-        acc
-      )
-    else
-      {:ok, acc}
-    end
-  end
 
   defp do_reply_to_review_thread(request_fun, token, thread_id, body, max_attempts, opts, attempt) do
     case add_review_thread_reply(request_fun, token, thread_id, body) do
@@ -704,7 +557,7 @@ defmodule Aiur.GitHub.Client do
   defp known_pat_permission_message?(_message), do: false
 
   defp verify_review_thread_resolution_ready(request_fun, token, thread_id, terminal_reply_body, opts) do
-    case fetch_review_thread(request_fun, token, thread_id) do
+    case ReviewThreads.fetch_review_thread(request_fun, token, thread_id) do
       {:ok, thread_body} ->
         Logger.info("GitHub review thread resolution verification response: #{inspect(thread_body)}")
 
@@ -729,7 +582,7 @@ defmodule Aiur.GitHub.Client do
          terminal_reply_body,
          opts
        ) do
-    case fetch_review_thread(request_fun, token, thread_id) do
+    case ReviewThreads.fetch_review_thread(request_fun, token, thread_id) do
       {:ok, thread_body} ->
         Logger.info("GitHub review thread post-resolution verification response: #{inspect(thread_body)}")
 
@@ -760,8 +613,8 @@ defmodule Aiur.GitHub.Client do
          opts,
          phase \\ :before_resolve
        ) do
-    thread = review_thread_from_body(thread_body)
-    latest = thread |> thread_comments() |> List.last()
+    thread = ReviewThreads.review_thread_from_body(thread_body)
+    latest = thread |> ReviewThreads.thread_comments() |> List.last()
 
     with {:ok, bot_account} <- BotIdentity.bot_account(opts, request_fun, token) do
       cond do
@@ -785,7 +638,7 @@ defmodule Aiur.GitHub.Client do
              %{
                expected: bot_account,
                actual: get_in(latest, ["author", "login"]),
-               latest_comment: normalize_verified_thread_comment(latest)
+               latest_comment: ReviewThreads.normalize_verified_thread_comment(latest)
              }
            )}
 
@@ -797,7 +650,7 @@ defmodule Aiur.GitHub.Client do
              %{
                expected: terminal_reply_body,
                actual: Map.get(latest, "body"),
-               latest_comment: normalize_verified_thread_comment(latest)
+               latest_comment: ReviewThreads.normalize_verified_thread_comment(latest)
              }
            )}
 
@@ -805,7 +658,7 @@ defmodule Aiur.GitHub.Client do
           {:ok,
            %{
              "review_thread_id" => thread_id,
-             "latest_comment" => normalize_verified_thread_comment(latest)
+             "latest_comment" => ReviewThreads.normalize_verified_thread_comment(latest)
            }}
 
         true ->
@@ -854,10 +707,13 @@ defmodule Aiur.GitHub.Client do
 
   defp review_thread_authoritative_comment?(thread, opts) when is_map(thread) do
     opts = BotIdentity.codeowners_classification_opts(opts)
-    context = thread |> normalize_thread_for_comment_context() |> thread_ownership_context(opts)
+    context =
+      thread
+      |> normalize_thread_for_comment_context()
+      |> ReviewThreads.thread_ownership_context(opts)
 
     thread
-    |> thread_comments()
+    |> ReviewThreads.thread_comments()
     |> Enum.reverse()
     |> Enum.find(fn comment ->
       author = get_in(comment, ["author", "login"])
@@ -879,7 +735,7 @@ defmodule Aiur.GitHub.Client do
   end
 
   defp verify_review_thread_reply(request_fun, token, thread_id, body, opts) do
-    case fetch_review_thread(request_fun, token, thread_id) do
+    case ReviewThreads.fetch_review_thread(request_fun, token, thread_id) do
       {:ok, thread_body} ->
         Logger.info("GitHub review thread reply verification response: #{inspect(thread_body)}")
 
@@ -897,12 +753,12 @@ defmodule Aiur.GitHub.Client do
     end
   end
 
-  defp fetch_review_thread(request_fun, token, thread_id) do
-    Transport.github_graphql(request_fun, token, @review_thread_query, %{"id" => thread_id})
-  end
-
   defp verify_latest_review_thread_comment(thread_body, thread_id, body, request_fun, token, opts) do
-    latest = thread_body |> review_thread_from_body() |> thread_comments() |> List.last()
+    latest =
+      thread_body
+      |> ReviewThreads.review_thread_from_body()
+      |> ReviewThreads.thread_comments()
+      |> List.last()
 
     with {:ok, bot_account} <- BotIdentity.bot_account(opts, request_fun, token) do
       cond do
@@ -919,7 +775,7 @@ defmodule Aiur.GitHub.Client do
           {:ok,
            %{
              "review_thread_id" => thread_id,
-             "latest_comment" => normalize_verified_thread_comment(latest)
+             "latest_comment" => ReviewThreads.normalize_verified_thread_comment(latest)
            }}
       end
     end
@@ -941,25 +797,6 @@ defmodule Aiur.GitHub.Client do
     }
 
     {:error, {:review_thread_latest_comment_body_mismatch, detail}}
-  end
-
-  defp review_thread_from_body(body) when is_map(body) do
-    case get_in(body, ["data", "node"]) do
-      %{"id" => _id} = thread -> thread
-      _ -> %{}
-    end
-  end
-
-  defp normalize_verified_thread_comment(comment) when is_map(comment) do
-    %{
-      "id" => Map.get(comment, "databaseId") || Map.get(comment, "id"),
-      "node_id" => Map.get(comment, "id"),
-      "body" => Map.get(comment, "body") || "",
-      "created_at" => Map.get(comment, "createdAt"),
-      "updated_at" => Map.get(comment, "updatedAt") || Map.get(comment, "createdAt"),
-      "html_url" => Map.get(comment, "url"),
-      "user" => %{"login" => get_in(comment, ["author", "login"])}
-    }
   end
 
   defp retryable_review_thread_verification_error?({:github, kind, _detail})
@@ -1018,93 +855,6 @@ defmodule Aiur.GitHub.Client do
     do: value
 
   defp normalize_non_negative_integer(_value, default), do: default
-
-  defp review_threads_page(body) when is_map(body) do
-    threads = get_in(body, ["data", "repository", "pullRequest", "reviewThreads", "nodes"])
-    page_info = get_in(body, ["data", "repository", "pullRequest", "reviewThreads", "pageInfo"])
-
-    if is_list(threads) and is_map(page_info) do
-      {:ok, {threads, page_info}}
-    else
-      {:error, :review_threads_missing}
-    end
-  end
-
-  defp unaddressed_thread_comments(threads, opts) when is_list(threads) do
-    threads
-    |> Enum.flat_map(&unaddressed_thread_comment(&1, opts))
-  end
-
-  defp unaddressed_thread_comment(%{"isResolved" => false} = thread, opts) do
-    thread
-    |> thread_comments()
-    |> List.last()
-    |> classify_thread_comment(thread, opts)
-    |> case do
-      %{authoritative: true} = comment ->
-        [comment]
-
-      %{} = comment ->
-        if unresolved_agent_review_thread_reply?(comment, opts),
-          do: [mark_review_thread_resolution_required(comment)],
-          else: []
-
-      _ ->
-        []
-    end
-  end
-
-  defp unaddressed_thread_comment(_thread, _opts), do: []
-
-  defp thread_comments(thread) when is_map(thread) do
-    case get_in(thread, ["comments", "nodes"]) do
-      comments when is_list(comments) -> comments
-      _ -> []
-    end
-  end
-
-  defp classify_thread_comment(nil, _thread, _opts), do: nil
-
-  defp classify_thread_comment(comment, thread, opts) when is_map(comment) and is_map(thread) do
-    normalized = normalize_thread_comment(comment, thread)
-    classification_opts = BotIdentity.codeowners_classification_opts(opts)
-    context = thread_ownership_context(normalized, classification_opts)
-    Codeowners.classify_comment(normalized, context, classification_opts)
-  end
-
-  defp normalize_thread_comment(comment, thread) do
-    path = Map.get(thread, "path")
-
-    %{
-      "id" => Map.get(comment, "databaseId"),
-      "review_thread_id" => Map.get(thread, "id"),
-      "body" => Map.get(comment, "body") || "",
-      "created_at" => Map.get(comment, "createdAt"),
-      "updated_at" => Map.get(comment, "updatedAt") || Map.get(comment, "createdAt"),
-      "html_url" => Map.get(comment, "url"),
-      "path" => path,
-      "line" => Map.get(thread, "line"),
-      "user" => %{"login" => get_in(comment, ["author", "login"])}
-    }
-  end
-
-  defp thread_ownership_context(%{"path" => path}, opts) when is_binary(path) and path != "" do
-    Codeowners.ownership_for_path(path, opts)
-  end
-
-  defp thread_ownership_context(_comment, opts), do: Codeowners.repo_ownership(opts)
-
-  defp unresolved_agent_review_thread_reply?(comment, opts) when is_map(comment) do
-    comment
-    |> get_in(["user", "login"])
-    |> BotIdentity.agent_login?(opts)
-  end
-
-  defp mark_review_thread_resolution_required(comment) do
-    comment
-    |> Map.put(:authoritative, true)
-    |> Map.put("review_thread_resolution_required", true)
-  end
 
   defp do_update_issue_state(update_context, state_name) do
     new_label = StatePolicy.state_label(update_context.prefix, state_name)
