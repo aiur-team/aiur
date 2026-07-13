6178a5cfbaf025982523f07b4e542f6e8e1e030c
everdred <kevinw@oplabs.co>
2026-07-13 00:50:43 -0700

    Add function A type spec

--- src/lib/aiur/sandbox/event_flow_demo.ex
+++ src/lib/aiur/sandbox/event_flow_demo.ex
@@ -4,5 +4,6 @@ defmodule Aiur.Sandbox.EventFlowDemo do
   # the test tickets flesh this out during the manual --test workflow;
   # `aiur --test` restores this file to HEAD before each fresh run.
 
+  @spec function_a() :: integer()
   def function_a, do: 42
 end
