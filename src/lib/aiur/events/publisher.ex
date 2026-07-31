206e80c4aeb677fb585def1a4483d197b9983ff0
Kevin Weaver <kevinweaver2@gmail.com>
2026-07-31 16:18:17 -0700

    Simplify merge event filter

--- src/lib/aiur/events/publisher.ex
+++ src/lib/aiur/events/publisher.ex
@@ -100,7 +100,7 @@ defmodule Aiur.Events.Publisher do
       executor_topic_from_github?(topic, payload, opts) ->
         {:error, :executor_namespace_rejects_github_source}
 
-      bot_self_loop?(actor) and not authoritative_merge_topic?(topic) ->
+      filtered_bot_self_loop?(topic, actor) ->
         :filtered
 
       not Keyword.get(opts, :bypass_contamination, false) and
@@ -289,6 +289,9 @@ defmodule Aiur.Events.Publisher do
   end
 
   defp authoritative_merge_topic?(topic), do: String.ends_with?(topic, ".pr.merged")
+
+  defp filtered_bot_self_loop?(topic, actor),
+    do: bot_self_loop?(actor) and not authoritative_merge_topic?(topic)
 
   defp tracked?(nil), do: true
 
